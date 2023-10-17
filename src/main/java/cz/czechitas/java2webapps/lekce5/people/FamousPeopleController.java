254ef2401b8d3c989d2278d81cd853804ee2604e
Filip Jirsák <filip@jirsak.org>
2023-10-17 22:18:52 +0200

    Vylepšení pro napovídání IDE.

--- src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce5/people/FamousPeopleController.java
@@ -2,7 +2,6 @@ package cz.czechitas.java2webapps.lekce5.people;
 
 import cz.czechitas.java2webapps.lekce5.entity.Gender;
 import cz.czechitas.java2webapps.lekce5.entity.Person;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author Filip Jirsák
@@ -35,7 +34,7 @@ public class FamousPeopleController {
   public ModelAndView detail(@PathVariable int id) {
     ModelAndView result = new ModelAndView("detail");
     result.addObject("person", service.getById(id));
-    result.addObject("gender", Gender.values());
+    result.addObject("gender", List.of(Gender.values()));
     return result;
   }
 
